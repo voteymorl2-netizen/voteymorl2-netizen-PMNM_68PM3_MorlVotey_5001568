d2fd252963595e2795e80071d3aad7120675f7bb
voteymorl <voteymorl2@gmail.com>
2026-05-20 22:40:12 +0700

    add login feature + middleware

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+require_once '../app/core/App.php';
+require_once '../app/middleware.php';
+
+$middleware = new Middleware();
+$middleware->checkLogin();
+
+$app = new App();
+
+?>
